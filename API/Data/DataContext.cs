196a8dcf497d22fdf351a806593f6f387616a5b1
Savaş Düzgün <savas_4242@hotmail.com>
2025-01-31 17:53:23 +0300

    product için DbSet yazıldı.

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,6 +1,9 @@
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
 namespace API.Data;
 
-public class DataContext
+public class DataContext : DbContext
 {
-    
+    public DbSet<Product> Products => Set<Product>();
 }

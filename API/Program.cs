fa19d552379caadf4b3eb4c16458830efe60618a
Savaş Düzgün <savas_4242@hotmail.com>
2025-02-04 15:06:34 +0300

    DataContext program.cs e eklendi.

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,9 @@
+using API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();

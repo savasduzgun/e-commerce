de590447a2667faba3d2a9c4234086f3ee92f0d2
Savaş Düzgün <savas_4242@hotmail.com>
2025-02-05 16:21:10 +0300

    migration uygulandı database oluşturuldu.

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c732ea37d52be7252e2633a0f9e13276a01bc154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1463ce1daeb3fb5d721404fa4e818fbdb0170e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

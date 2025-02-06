de099ecd1cca1b1e642c4cdc26b56873d18737eb
Savaş Düzgün <savas_4242@hotmail.com>
2025-02-06 15:36:12 +0300

    OnModelCraeting metodu yazıldı migration oluşturuldu update database yapıldı.

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -6,4 +6,18 @@ namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>().HasData(
+            new List<Product> {
+                new Product {Id=1, Name="IPhone 15", Description="Telefon açıklaması", ImageUrl="1.jpg", Price=70000, IsActive=true, Stock=100},
+                new Product {Id=2, Name="IPhone 16", Description="Telefon açıklaması", ImageUrl="2.jpg", Price=80000, IsActive=true, Stock=100},
+                new Product {Id=3, Name="IPhone 16 Pro", Description="Telefon açıklaması", ImageUrl="3.jpg", Price=90000, IsActive=false, Stock=100},
+                new Product {Id=4, Name="IPhone 16 Pro Max", Description="Telefon açıklaması", ImageUrl="4.jpg", Price=100000, IsActive=true, Stock=100}
+            }
+        );
+    }
 }
---
+++ API/Migrations/20250206123022_TestData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsActive", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "Telefon açıklaması", "1.jpg", true, "IPhone 15", 70000m, 100 },
+                    { 2, "Telefon açıklaması", "2.jpg", true, "IPhone 16", 80000m, 100 },
+                    { 3, "Telefon açıklaması", "3.jpg", false, "IPhone 16 Pro", 90000m, 100 },
+                    { 4, "Telefon açıklaması", "4.jpg", true, "IPhone 16 Pro Max", 100000m, 100 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
